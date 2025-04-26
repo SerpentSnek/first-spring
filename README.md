abf0792b2bb760d82c384e93d62ea6476cb38e0a
Kris Wong <37485546+SerpentSnek@users.noreply.github.com>
2025-04-25 20:28:57 -0700

    Update project structure and dependencies (got rid of docker for now); add hello world endpoint; project runs on localhost now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # first-spring
+## first task: create a hello world (done)
+Things of note:
+- this project uses Maven to build
+- check `pom.xml` for the list of dependencies
--- firstspringdemo/src/main/java/firstspring/firstspringdemo/FirstspringdemoApplication.java
+++ firstspringdemo/src/main/java/firstspring/firstspringdemo/FirstspringdemoApplication.java
@@ -2,9 +2,17 @@ package firstspring.firstspringdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstspringdemoApplication {
+
+    @GetMapping("/hello")
+    public String check() {
+        return "Hello World";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstspringdemoApplication.class, args);
